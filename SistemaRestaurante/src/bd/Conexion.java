2005b0e8065182887c18a119abed9e5c386aec6b
julskhana <juls.tier5@gmail.com>
2017-01-18 22:55:07 -0500

    agregacion de paquete de funciones para base de datos

---
+++ SistemaRestaurante/src/bd/Conexion.java
@@ -0,0 +1,173 @@
+package bd;
+
+//import entidades.Universidad;
+//import entidades.Usuario;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Conexion {
+    
+       private Connection con;
+       private static final String DRIVER = "com.mysql.jdbc.Driver";
+       private static final String DBMS = "mysql";
+       private static final String HOST = "127.0.0.1";
+       private static final String PORT = "3306";
+       private static final String DATABASE = "base_proyecto_java_20162s";
+       private static final String USER = "root";
+       private static final String PASSWORD = "";
+
+    void Conexion(){}
+    
+    /*METODO CONECTAR*/
+   
+    public void conectar ()throws Exception{
+        try {
+            Class.forName(DRIVER);
+        }catch(ClassNotFoundException ce){
+        }
+        
+            try{
+                this.con = DriverManager.getConnection("jdbc:" + DBMS + "://" + HOST + ":" + PORT + "/" + DATABASE, USER, PASSWORD);                
+                System.out.println("CONEXION EXITOSA CON LA BASE DE DATOS");
+            }catch(SQLException exception){
+                System.out.println("ERROR: NO SE PUDO CONECTAR CON LA BASE DE DATOS: "+exception);
+            }             
+    }
+    
+    public boolean desconectar()
+    {
+        try
+        {
+            this.con.close();
+            return(true);
+        }
+        catch(Exception e)
+        {
+            return(false);
+        }    
+    }
+    
+    public boolean ingresarUniversidad(Universidad u)
+    {
+        try
+        {
+                PreparedStatement st = null;
+                st = con.prepareStatement("INSERT INTO universidad(nombre,direccion,ciudad,telefono) VALUES(?,?,?,?) ");            
+                st.setString(1,u.getNombre());
+                st.setString(2,u.getDireccion());
+                st.setString(3,u.getCiudad());
+                st.setString(4,u.getTelefono());
+                st.executeUpdate();
+                st.close();                        
+            return true;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return false;
+        }        
+    }
+    
+    public ArrayList<Universidad> consultarUniversidades(String descripcion, String tipo)
+    {
+        ArrayList<Universidad> universidades = new ArrayList<Universidad>();
+
+        try
+        {
+            Statement st = this.con.createStatement();
+            ResultSet rs = null;  
+            
+            if(tipo.equalsIgnoreCase("TODOS")){
+                rs = st.executeQuery("SELECT * FROM universidad;");
+            }else if(tipo.equalsIgnoreCase("ID")){
+                rs = st.executeQuery("SELECT * FROM universidad WHERE id = "+descripcion+";");
+            }else{
+                rs = st.executeQuery("SELECT * FROM universidad WHERE "+tipo+" LIKE '%"+descripcion+"%';");
+            }            
+                        
+            while(rs.next())
+            {
+                int id              = rs.getInt("id");
+                String nombre       = rs.getString("nombre");
+                String direccion    = rs.getString("direccion");
+                String ciudad       = rs.getString("ciudad");
+                String telefono     = rs.getString("telefono");
+                Universidad u = new Universidad(id, nombre, direccion, ciudad, telefono);                
+                universidades.add(u);
+            }
+        }
+        catch(Exception e){System.out.println(e);}
+     return(universidades);
+    } 
+    
+    
+    public boolean modificarUniversidad(Universidad u)
+    {
+        try
+        {
+            PreparedStatement st = null;
+            st = con.prepareStatement("UPDATE universidad SET nombre = ?, ciudad = ?, direccion = ?, telefono = ? WHERE id = ?");                        
+            st.setString(1,u.getNombre());
+            st.setString(2,u.getCiudad());
+            st.setString(3,u.getDireccion());
+            st.setString(4,u.getTelefono());
+            st.setInt(5,u.getId());                       
+            st.executeUpdate();
+            st.close();                        
+            return true;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return false;
+        }
+        
+    }   
+    
+    public boolean eliminarUniversidad(int id)
+    {
+        try
+        {
+            PreparedStatement st = null;
+            st = con.prepareStatement("DELETE FROM universidad WHERE id = ?");            
+            st.setInt(1,id);         
+            st.executeUpdate();
+            st.close();                        
+            return true;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+            return false;
+        }        
+    } 
+   
+    
+    public boolean esUsuarioValido(Usuario u)
+    {        
+        boolean resultado = false;
+        ResultSet rs = null;                       
+        PreparedStatement st = null;
+        try
+        {            
+            st = con.prepareStatement("SELECT * FROM usuario WHERE id = ? AND clave = md5(?) AND estado = ?");            
+            st.setString(1,u.getId());         
+            st.setString(2,u.getClave());
+            st.setString(3,"A");
+            rs = st.executeQuery();            
+            if(rs.next()){
+                u.setRol(rs.getString("rol").charAt(0));
+                resultado = true;
+            } 
+            
+            rs.close();
+            st.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            resultado = false;
+        }           
+     return resultado; 
+    }
+    
+}
