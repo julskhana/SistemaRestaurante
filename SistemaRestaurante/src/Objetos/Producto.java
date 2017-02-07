6b466bf70d9e1a657f3b100fcac8c23061656719
julskhana <juls.tier5@gmail.com>
2017-02-07 10:29:59 -0500

    cambios a producto

--- SistemaRestaurante/src/Objetos/Producto.java
+++ SistemaRestaurante/src/Objetos/Producto.java
@@ -5,10 +5,33 @@
  */
 package Objetos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Julian
  */
 public class Producto {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private String tamaño;
+    private float precio;
+    private String tipo;
+    private ArrayList<Ingrediente> ingredientes;
+    
+    //constructor normal
+    public Producto(int id,String nombre,String descripcion,String tamaño,float precio,String tipo,ArrayList<Ingrediente> ing){
+        this.id=id;
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.tamaño=tamaño;
+        this.precio=precio;
+        this.tipo=tipo;
+        this.ingredientes=ing;
+    }
+    
+    //constructor vacio
+    public Producto(){}
     
 }
